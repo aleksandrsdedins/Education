47dcb858ecb96a02645435f9f10cbc0a9fa553ff
Alexandr <aleksandrsdedins@gmail.com>
2021-05-10 20:14:25 +0300

    Lesson 14

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ personalMovieDB.movie[c] = d;
 console.log(personalMovieDB); */
 
 // Lesson 13
-
+/*
 if (4 == 4) {
     console.log('Ok!');
 } else {
@@ -38,3 +38,63 @@ if (num < 49) {
 } else {
     console.log('Ok!');
 }
+
+(num === 50) ? console.log('Ok!') : console.log('Error');
+
+
+const num = 50;
+
+switch (num) {
+    case 49: 
+        console.log('Error');
+        break;
+    case 100:
+        console.log('Error');
+        break;
+    case 5: 
+        console.log('Right');
+        break;
+    default:
+        console.log('Maybe later');
+        break;
+}
+*/
+
+// Lesson 14
+/*
+let num = 50;
+
+while (num <= 55) {
+    console.log(num);
+    num++;
+}
+
+
+
+let num = 50;
+
+do {
+    console.log(num);
+    num++;
+} 
+while (num < 55);
+
+
+let num = 50;
+
+for (let i = 1; i < 8; i++) {
+    console.log(num);
+    num++;
+}
+
+
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+ //       break;
+        continue;
+    }
+        console.log(i);
+}
+
+*/
+
